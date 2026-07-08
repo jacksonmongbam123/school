60b61eb3a32f2c4f9628b0f2b05178bd771afc3b
Jackson Mongbam <jacksonmongbam123@gmail.com>
2026-07-08 17:15:59 +0000

    revert: remove /rel_teacher_qualifications path on the server

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use("/timetable", timetableRoutes);
 app.use("/df", dfRoutes);
 app.use("/m", mRoutes);
 app.use("/rel", relRoutes);
-app.use("/rel_teacher_qualifications", require("./routes/rel/teacher_qualification_routes"));
 
 // Database configuration
 const configs = require('./config/config');
